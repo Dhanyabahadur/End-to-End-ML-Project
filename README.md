e22d88b7359355dc623cca442d15250f8dd86bc8
Dhanyabahadur <dhanyabahadur2000@gmail.com>
2024-02-23 12:46:24 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ conda activate mlproj
 ```bash
 pip install -r requirements.txt
 ```
+
+## Workflows
+
+1. update config.yaml
+2. update schema.yaml
+3. update params.yaml
+4. update the entity
+5. update the configuration manager in src config
+6. update the components
+7. update the pipeline
+8. update the main.py
+9. update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Dhanya!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<\n\nx==================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
